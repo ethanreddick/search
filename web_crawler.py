7ecc2959e20b6efee71513b9f4fb2127e69ac5ad
justinn0725 <86483095+justinn0725@users.noreply.github.com>
2023-09-14 13:32:34 -0400

    web crawler

--- web_crawler.py
+++ web_crawler.py
@@ -1,19 +1,43 @@
+import random
 import requests
 from bs4 import BeautifulSoup
 
-def search():
-    query = search_bar.text()
-    bias = bias_selector.currentText()
-    results = crawl_web(query)  # Call the web crawling function
-    results_display.setPlainText(f"Search query: {query}\nSearch bias: {bias}\n\nResults:\n{results}")
+# List of user-agent strings
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/100.0.0.0 Safari/537.36",
+    # Add more user agents as needed
+]
 
+# List of proxy servers (example, replace with actual proxy addresses)
+proxies = [
+    'http://proxy1.example.com:8080',
+    'http://proxy2.example.com:8080',
+    # Add more proxy addresses as needed
+]
+
+# Function to get a random user-agent
+def get_random_user_agent():
+    return random.choice(user_agents)
+
+# Function to get a random proxy
+def get_random_proxy():
+    return random.choice(proxies)
+
+# Modify the crawl_web function to set user-agent and proxy for each request
 def crawl_web(query):
     # Use Google search URL
     search_url = f"https://www.google.com/search?q={query}"
     
     try:
-        # Send an HTTP GET request to the search URL
-        response = requests.get(search_url)
+        # Randomly select a user-agent for this request
+        headers = {'User-Agent': get_random_user_agent()}
+        
+        # Randomly select a proxy for this request
+        proxy = {'http': get_random_proxy()}
+        
+        # Send an HTTP GET request with the selected user-agent and proxy
+        response = requests.get(search_url, headers=headers, proxies=proxy)
         response.raise_for_status()  # Check for any request errors
         
         # Parse the HTML content using BeautifulSoup
