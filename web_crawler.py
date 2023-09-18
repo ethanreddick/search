e4393994a571842bbf5e6ee7ee7a80c0d763c3f6
justinn0725 <86483095+justinn0725@users.noreply.github.com>
2023-09-18 19:31:08 -0400

    web crawler

--- web_crawler.py
+++ web_crawler.py
@@ -1,6 +1,7 @@
 import random
 import requests
 from bs4 import BeautifulSoup
+import time
 
 # List of user-agent strings
 user_agents = [
@@ -16,6 +17,12 @@ proxies = [
     # Add more proxy addresses as needed
 ]
 
+# Define a constant for the maximum number of requests per minute
+MAX_REQUESTS_PER_MINUTE = 60
+
+# Keep track of the last request time
+last_request_time = None
+
 # Function to get a random user-agent
 def get_random_user_agent():
     return random.choice(user_agents)
@@ -24,12 +31,23 @@ def get_random_user_agent():
 def get_random_proxy():
     return random.choice(proxies)
 
-# Modify the crawl_web function to set user-agent and proxy for each request
+# Function to check and enforce rate limiting
+def enforce_rate_limit():
+    global last_request_time
+    if last_request_time is not None:
+        elapsed_time = time.time() - last_request_time
+        if elapsed_time < 60:  # Less than 60 seconds (1 minute)
+            time.sleep(60 - elapsed_time)  # Wait to ensure rate limit compliance
+    last_request_time = time.time()
+
+# Modify the crawl_web function to set user-agent, proxy, and enforce rate limiting for each request
 def crawl_web(query):
     # Use Google search URL
     search_url = f"https://www.google.com/search?q={query}"
     
     try:
+        enforce_rate_limit()  # Enforce rate limiting before making the request
+        
         # Randomly select a user-agent for this request
         headers = {'User-Agent': get_random_user_agent()}
         
@@ -48,6 +66,37 @@ def crawl_web(query):
         
         # Format and return the results
         results = "\n".join([title.text for title in result_titles])
+        return results
+    
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred: {str(e)}"
+
+# Sample modification to handle IMDb search results (you can adapt this for other websites)
+def crawl_imdb(query):
+    # Use IMDb search URL
+    search_url = f"https://www.imdb.com/find?q={query}&s=tt"
+    
+    try:
+        enforce_rate_limit()  # Enforce rate limiting before making the request
+        
+        # Randomly select a user-agent for this request
+        headers = {'User-Agent': get_random_user_agent()}
+        
+        # Randomly select a proxy for this request
+        proxy = {'http': get_random_proxy()}
+        
+        # Send an HTTP GET request with the selected user-agent and proxy
+        response = requests.get(search_url, headers=headers, proxies=proxy)
+        response.raise_for_status()  # Check for any request errors
+        
+        # Parse the HTML content using BeautifulSoup for IMDb search results
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Extract search result titles from IMDb (specific to IMDb's structure)
+        result_titles = soup.find_all('td', class_='result_text')
+        
+        # Format and return the results (specific to IMDb)
+        results = "\n".join([title.text.strip() for title in result_titles])
         return results
     
     except requests.exceptions.RequestException as e:
